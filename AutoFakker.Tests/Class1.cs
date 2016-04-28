93fa10102d1ae42e173af1815ee5d7cf9369611d
michal-ciechan <lndcobra@hotmail.com>
2016-04-28 22:55:30 +0100

    Initial Version

---
+++ AutoFakker.Tests/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AutoFakker.Tests
+{
+    [TestFixture]
+    public class AutoFakeTest
+    {
+        private AutoFaker _fake;
+
+        [SetUp]
+        public void AAA_TestSetup()
+        {
+            _fake = new AutoFaker();
+        }
+
+        [Test]
+        public void Create_RootClass_Returns_TypeAssignableToRootClass()
+        {
+            var fake = _fake.Create<RootClass>();
+            
+            fake.Should().BeAssignableTo<RootClass>();
+        }
+
+        [Test]
+        public void Create_RootClass_Returns_TypeAssignableToFakeRooClass()
+        {
+            var fake = _fake.Create<RootClass>();
+            
+            fake.Should().BeAssignableTo<Fake<RootClass>>();
+        }
+    }
+
+    public class RootClass
+    {
+        public void Tree()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AutoFakker/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FakeItEasy;
+
+namespace AutoFakker
+{
+    public static class F
+    {
+    }
+
+    public class AutoFaker
+    {
+        private ClassFaker _class;
+
+        public AutoFaker()
+        {
+            _class = new ClassFaker();
+        }
+
+        public T Create<T>()
+        {
+            var fake = A.Fake<T>();
+            
+            return fake;
+        }
+    }
+
+    public class ClassMemberFinder
+    {
+        
+    }
+
+    public class ClassFaker
+    {
+
+    }
+
+
+}
